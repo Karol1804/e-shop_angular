53d86fdd5c0d43df659ac5bf294d787915d9c674
Karol Knapik <knapikkarol3@gmail.com>
2022-07-08 09:47:54 +0200

    step2 added all needed component...set routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
+import { LoginComponent } from './user/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckOutComponent } from './user/check-out/check-out.component';
 import { HomeComponent } from './user/home/home.component';
+import { OrderSuccessComponent } from './user/order-success/order-success.component';
+import { ProductsComponent } from './user/products/products.component';
 import { ShoppingCartComponent } from './user/shopping-cart/shopping-cart.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { MyOrdersComponent } from './user/my-orders/my-orders.component';
 
 const routes: Routes = [
-  {path:'home',component: HomeComponent},
+  {path:'',component: HomeComponent},
   {path:'shopping-cart',component: ShoppingCartComponent},
+  {path:'products',component: ProductsComponent},
+  {path:'check-out',component: CheckOutComponent},
+  {path:'order-success',component: OrderSuccessComponent},
+  {path:'my/orders',component: MyOrdersComponent},
+  {path:'login',component: LoginComponent},
+  {path:'admin/products',component: AdminProductsComponent},
+  {path:'admin/orders',component: AdminOrdersComponent},
+ 
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './user/home/home.component';
 import { NavigationComponent } from './user/navigation/navigation.component';
 import { ShoppingCartComponent } from './user/shopping-cart/shopping-cart.component';
+import { MyOrdersComponent } from './user/my-orders/my-orders.component';
+import { ProductsComponent } from './user/products/products.component';
+import { CheckOutComponent } from './user/check-out/check-out.component';
+import { OrderSuccessComponent } from './user/order-success/order-success.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './user/login/login.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +21,13 @@ import { ShoppingCartComponent } from './user/shopping-cart/shopping-cart.compon
     NavigationComponent,
     HomeComponent,
     ShoppingCartComponent,
+    MyOrdersComponent,
+    ProductsComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
